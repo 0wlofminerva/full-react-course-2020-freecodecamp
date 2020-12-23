2f8f9694b03aa2aa90f9580e0ca1845ee0e75dbe
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-23 10:03:18 +0100

    JSX rules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDom from 'react-dom';
 
-// stateless functional component
-// always return JSX
+// nested Components, React Tools
 
 function Greeting() {
-	return <h4>this is john and this is my first component</h4>;
+	return (
+		<div>
+			<Person />
+			<Message />
+		</div>
+	);
 }
+
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+	return <p>this is my message</p>;
+};
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
